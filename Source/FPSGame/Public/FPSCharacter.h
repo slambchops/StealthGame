12ab059bb5affee4cab93c26fdb82b0710cd9427
Josh Elliott <jelliott.uf@gmail.com>
2019-08-06 20:29:15 -0500

    Make player movement synced across server and clients

--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -59,6 +59,9 @@ protected:
 	/** Fires a projectile. */
 	void Fire();
 
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerFire();
+
 	/** Handles moving forward/backward */
 	void MoveForward(float Val);
 
@@ -74,5 +77,6 @@ public:
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
 
+	virtual void Tick(float DeltaTime) override;
 };
 
